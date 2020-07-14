04a964a7f76b75d00d02cfad4579ae59c6669440
Nobuhito Iwata <nobuhito323@gmail.com>
2020-07-14 20:48:58 +0900

    update:type-inference.ts

--- src/06_type-inference.ts
+++ src/06_type-inference.ts
@@ -7,42 +7,41 @@ export {}; // モジュール化をすることで、変数のダブりを回避
 //------------------------
 // let
 //------------------------
-let a = 0; //let a: number
-let b = "Taro"; //let b: string
-let c = false; //let c: boolean
-let d; //let c: boolean
-let e = a + 5; //let e: number
-let f = a + "5"; //let f: number
-let g = b + 5; //let g: string
-let h = b + "5"; //let h: string
-let i: number = 0; //let i: number
-let j: string = "Taro"; //let j: string
-let k: boolean = false; //let k: boolean
-console.log(a);
-console.log(b);
-console.log(c);
-console.log(d);
-console.log(e);
-console.log(f);
-console.log(g);
-console.log(h);
-console.log(i);
-console.log(j);
-console.log(k);
+// let a = 0; //let a: number
+// let b = "Taro"; //let b: string
+// let c = false; //let c: boolean
+// let d; //let d: boolean
+// let e = a + 5; //let e: number
+// let f = a + "5"; //let f: string
+// let g = b + 5; //let g: string
+// let h = b + "5"; //let h: string
+// let i = c + 5; //NG
+// let j = c + "5"; //let j: string
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+// console.log(f);
+// console.log(g);
+// console.log(h);
+// console.log(i);
+// console.log(j);
 
 //------------------------
 // const
 //------------------------
-// const a = 0;  //const a: 0
+// const a = 0; //const a: 0
 // const b = "Taro"; //const b: "Taro"
-// const c = false;  //const c: false
-// const d = a + 5;  //const d: number
-// const e = a + "5";  //const e: string
-// const f = b + 5;  //const f: string
-// const g = b + "5";  //const g: string
-// const h: number = 0;  //const h: number
-// const i: string = "Taro"; //const h: number
-// const j: boolean = false; //const h: number
+// const c = false; //const c: false
+// const d; //NG
+// const e = a + 5; //const e: number
+// const f = a + "5"; //const f: string
+// const g = b + 5; //const g: string
+// const h = b + "5"; //const h: string
+// const i = c + 5; //NG
+// const j = c + "5"; //const j: string
 
 // console.log(a);
 // console.log(b);
@@ -58,14 +57,14 @@ console.log(k);
 //------------------------
 // Widening Literal Types
 //------------------------
-// const a = 0;  //const a: 0
+// const a = 0; //const a: 0
 // const b: 0 = 0; //const b: 0
 // const c = 0 as 0; //const c: 0
 
-// let d = 0;  //let d: number
-// let e = a;  //let d: number
-// let f = b;  //let f: 0
-// let g = c;  //let g: 0
+// let d = 0; //let d: number
+// let e = a; //let e: number
+// let f = b; //let f: 0
+// let g = c; //let g: 0
 
 //------------------------
 // Array
@@ -103,9 +102,68 @@ console.log(k);
 // const a = [false, 1] as [boolean, number]; //const a: [boolean, number]
 // const b = ["Taro", false, 1] as [string, boolean, number]; //const b: [string, boolean, number]
 // const c = b[0]; //const c: string
-// const d = b[1]; //const c: string
+// const d = b[1]; //const d: boolean
 // const e = b[2]; //const e: number
 // const f = b[3]; //NG
 // a.push(true); //OK
-// a.push(2) //OK
-// a.push("Taro") //NG
+// a.push(2); //OK
+// a.push("Taro"); //NG
+
+//------------------------
+// Object
+//------------------------
+// const obj1 = {
+//   a: 0,
+//   b: false,
+//   c: "Taro",
+// };
+// const obj1: {
+//   a: number;
+//   b: boolean;
+//   c: string;
+// }
+
+// obj1.a = 10; //OK
+// obj1.a = true;  //NG
+// obj1.a = "Taro";  //NG
+
+//------------------------
+// 関数の戻り値
+//------------------------
+//function a(num: number): number
+function a(num: number) {
+  return num;
+}
+//function b(age: number): string
+function b(age: number) {
+  return `私は${age}歳です。`;
+}
+//function c(message: string): void
+function c(message: string) {
+  console.log(`Hello ${message}`);
+}
+//function d(num: number): "Taro" | "Jiro"
+function d(num: number) {
+  if (num > 0) {
+    return "Taro";
+  } else {
+    return "Jiro";
+  }
+}
+//function e(num: number): "Taro" | undefined
+//noImplicitReturns=Trueにするとエラー
+function e(num: number) {
+  if (num > 0) {
+    return "Taro";
+  }
+}
+//------------------------
+// 関数の引数
+//------------------------
+//function a(num: any): "Taro" | undefined
+// function a(num) {
+//   if (num > 0) {
+//     return "Taro";
+//   }
+// }
+// a(0);
